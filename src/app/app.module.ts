cfdb8d93c140457fc27a054ab822cd60f07f5c80
Edy Beingolea <edy.beingolea@vallegrande.edu.pe>
2023-03-31 11:11:09 -0700

    hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './heroes/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Conuntercomponent
+    Conuntercomponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
